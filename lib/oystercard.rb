65c19377a431a974c3ccc1b4cae571c2ee6250d0
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-04-06 16:05:13 +0100

    added touch in / touch out / in journey methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,10 +5,28 @@ class Oystercard
 
   def initialize(balance = DEFAULT_VALUE)
     @balance = balance
+    @in_use = false
   end
 
   def top_up(money)
     raise "Error: New balance over £#{MAX_VALUE}." if (@balance + money) > MAX_VALUE
     @balance += money
   end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def touch_in
+    @in_use = true
+  end
+
+  def touch_out
+    @in_use = false
+  end
+
+  def in_journey?
+    @in_use
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,10 +5,6 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  it "responds to the top up method" do
-    expect(subject).to respond_to(:top_up)
-  end
-
   it "adds money to balance" do
     subject.top_up(10)
     expect(subject.balance).to eq(10)
@@ -18,4 +14,30 @@ describe Oystercard do
     max = Oystercard::MAX_VALUE
     expect { subject.top_up(max + 1) }.to raise_error("Error: New balance over £#{max}.")
   end
+
+  it "deducts money from balance" do
+    subject.top_up(10)
+    subject.deduct(5)
+    expect(subject.balance).to eq(5)
+  end
+
+  it "responds to touch in" do
+    expect(subject).to respond_to(:touch_in)
+  end
+
+  it "starts journey when you touch in" do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
+
+  it "responds to touch out" do
+    expect(subject).to respond_to(:touch_out)
+  end
+  
+  it "ends journey when you touch out" do
+    subject.touch_in
+    subject.touch_out
+    expect(subject).not_to be_in_journey
+  end
+
 end
