dba712eb4f2475e42d25dc61c48156d09b165612
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-04-09 10:37:14 +0100

    Added journey class - started Step 14, incomplete

---
+++ spec/journey_spec.rb
@@ -0,0 +1,10 @@
+require "journey"
+
+describe Journey do
+  context "#initialize" do
+    it "should have an entry station do" do
+      expect(subject.entry_station).to eq(subject.entry_station)
+    end
+  end
+end
+
